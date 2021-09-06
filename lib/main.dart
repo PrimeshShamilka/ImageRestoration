f160962b7f071b0f51541549c589808979d5db32
PrimeshES <primesh.es@gmail.com>
2021-09-06 18:31:42 +0530

    tflite model loaded

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'UserImagePicker.dart';
+import 'package:tflite/tflite.dart';
+
+
+
+//void main() => runApp(MyApp());
+
+//class MyApp extends StatelessWidget{
+//  Widget build(BuildContext context){
+//    return MaterialApp(
+//      title: 'tflite',
+//      home: Scaffold(
+//        body: Text('tflite'),
+//      ),
+//    );
+//  }
+//}
+
+
 
 void main(){
   runApp(MaterialApp(
@@ -16,9 +34,32 @@ class ImageRestoration extends StatefulWidget{
 
 class _ImageRestorationState extends State<ImageRestoration>{
   File _userImageFile;
+  bool _busy;
 
   void _pickedImage(File image){
     _userImageFile = image;
+  }
+
+  loadTfModel() async {
+    await Tflite.loadModel(
+        model: "assets/models/ssd_mobilenet.tflite",
+        labels: "assets/models/labels.txt",
+        numThreads: 1,
+        isAsset: true,
+        useGpuDelegate: false
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _busy = true;
+    loadTfModel().then((val) {{
+      setState(() {
+        _busy = false;
+      });
+      print ('model loading finished');
+    }});
   }
 
   @override
